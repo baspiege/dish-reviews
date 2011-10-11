4bf6508556cb1ea43d4896b63580bfe773eb04cd
spiegel121@gmail.com <spiegel121@gmail.com@d47931f1-c9d7-4dc6-7dbc-dd6694c20093>
2011-10-11 02:45:00 +0000

    Review - update/delete

---
+++ trunk/src/geonotes/data/ReviewUpdate.java
@@ -0,0 +1,58 @@
+package geonotes.data;
+
+import java.util.Date;
+import java.util.Map;
+import javax.jdo.PersistenceManager;
+import javax.servlet.http.HttpServletRequest;
+
+import geonotes.data.model.Review;
+import geonotes.utils.DisplayUtils;
+import geonotes.utils.RequestUtils;
+
+/**
+ * Update a review.
+ *
+ * @author Brian Spiegel
+ */
+public class ReviewUpdate {
+
+    /**
+     * Update a review.
+	   *
+     * @param aRequest The request
+     *
+     * @since 1.0
+     */
+    public void execute(HttpServletRequest aRequest) {
+
+        // Get Id.
+        Long reviewId=(Long)aRequest.getAttribute("reviewId");
+        
+        // Fields
+        String note=(String)aRequest.getAttribute("note");
+        
+        PersistenceManager pm=null;
+        try {
+            pm=PMF.get().getPersistenceManager();
+                        
+            Review review=ReviewGetSingle.getReview(aRequest,pm,reviewId.longValue());
+            
+            if (review!=null){
+            
+                if (note!=null) {
+                    review.setNote(note);
+                }
+                    
+                review.setLastUpdateTime(new Date());
+            }
+        } catch (Exception e) {
+            System.err.println(this.getClass().getName() + ": " + e);
+            e.printStackTrace();
+            RequestUtils.addEditUsingKey(aRequest,"requestNotProcessedEditMsssage");
+        } finally {
+            if (pm!=null) {
+                pm.close();
+            }
+        }
+    }
+}
