49776cfb7cbee1258debf8744d67bf887f1c1e06
spiegel121 <spiegel121@gmail.com>
2011-10-24 03:37:16 +0000

    Show image of last review on dishes page.

---
+++ src/geonotes/ReviewLastThumbNailImage.java
@@ -0,0 +1,47 @@
+package geonotes;
+
+import java.io.IOException;
+import javax.jdo.PersistenceManager;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import geonotes.data.ReviewGetSingle;
+import geonotes.data.PMF;
+import geonotes.data.model.Review;
+import geonotes.utils.RequestUtils;
+
+/**
+* Return the thumbnail image of the last review for a dish.
+*/
+public class ReviewLastThumbNailImage extends HttpServlet {
+
+    /**
+    * Process the request.
+    */
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+     
+        // Get Id.
+        Long dishId=RequestUtils.getNumericInput(request,"dishId","dishId",true);
+
+        PersistenceManager pm=null;
+        try {
+            pm=PMF.get().getPersistenceManager();
+                        
+            Review review=ReviewGetSingle.getLastReview(request,pm,dishId.longValue());
+            
+            if (review!=null && review.imageThumbnail!=null){
+                response.setContentType("image/jpeg");
+                response.getOutputStream().write(review.imageThumbnail.getBytes());
+            }
+        } catch (Exception e) {
+            System.err.println(ReviewThumbNailImage.class.getName() + ": " + e);
+            e.printStackTrace();
+        } finally {
+            if (pm!=null) {
+                pm.close();
+            }
+        }
+    }
+}
