b2a06ed5626b984fefc91eda159831bedfd6abaa
spiegel121@gmail.com <spiegel121@gmail.com@d47931f1-c9d7-4dc6-7dbc-dd6694c20093>
2011-12-04 20:15:24 +0000

    Dishes reset.

--- trunk/src/geonotes/data/DishesUpdateAllUtil.java
+++ trunk/src/geonotes/data/DishesUpdateAllUtil.java
@@ -7,8 +7,6 @@ import javax.jdo.Query;
 import javax.servlet.http.HttpServletRequest;
 
 import geonotes.data.model.Dish;
-import geonotes.data.model.Review;
-import geonotes.utils.NumberUtils;
 import geonotes.utils.RequestUtils;
 
 /**
@@ -26,41 +24,32 @@ public class DishesUpdateAllUtil {
      */
     public void execute(HttpServletRequest aRequest) {
         PersistenceManager pm=null;
+        List<Dish> results=null;
         try {
             pm=PMF.get().getPersistenceManager();
+
             Query query=null;
             try {
-                
                 query = pm.newQuery(Dish.class);
-                
-                List<Dish> results = (List<Dish>) query.execute();
-                
+                results = (List<Dish>) query.execute();               
                 // Bug workaround.  Get size actually triggers the underlying database call.
                 results.size();
-                
-                for (Dish dish:results) {
-                
-                    //if (dish.yesVote==null) {
-                        dish.setYesVote(dish.vote);
-                    //}
-                
-                    // Reset note (to set lowercase)
-                    //dish.setNote(dish.note);
-                    
-                    // Set last image
-                    //Review review=ReviewGetSingle.getLastReviewWithImage(aRequest,pm,dish.getKey().getId());
-                    //if (review!=null) {
-                    //    dish.setLastReviewImageId(review.getKey().getId());
-                    //}
-                }
-                
-                // Set into request
-                aRequest.setAttribute("dishes", results);
             } finally {
                 if (query!=null) {
                     query.closeAll();
                 }
             }
+        } catch (Exception e) {
+            System.err.println(this.getClass().getName() + ": " + e);
+            e.printStackTrace();
+            RequestUtils.addEditUsingKey(aRequest,"requestNotProcessedEditMsssage");
+        }
+        
+        try {            
+            for (Dish dish:results) {
+                //dish.setYesVote(dish.vote);
+                dish.setYesVote(0l);                
+            }            
         } catch (Exception e) {
             System.err.println(this.getClass().getName() + ": " + e);
             e.printStackTrace();
