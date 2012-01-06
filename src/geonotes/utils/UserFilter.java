2344c503695f7ec09071d63bae23aaec8c4fbce2
spiegel121 <spiegel121@gmail.com>
2012-01-06 05:08:03 +0000

    Mid code - set user.

---
+++ src/geonotes/utils/UserFilter.java
@@ -0,0 +1,41 @@
+package geonotes.utils;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+* Grab the user from dishRevUser cookie and sets as user principal.
+*/
+public class UserFilter implements Filter {
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        // Cast to http servlet
+        HttpServletRequest httpServletRequest=(HttpServletRequest)servletRequest;
+
+        String user=null;
+        for (Cookie cookie: httpServletRequest.getCookies()) {
+            if (cookie.getName().equals("dishRevUser")) {
+                user=cookie.getValue();
+                break;
+            }
+        }
+        
+        filterChain.doFilter(new UserRoleRequestWrapper(user, httpServletRequest), servletResponse);
+    }
+    
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Nothing for now    
+    }
+    
+    public void destroy() {
+        // Nothing for now
+    }
+}
---
+++ src/geonotes/utils/UserRoleRequestWrapper.java
@@ -0,0 +1,35 @@
+package geonotes.utils;
+
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+/**
+ * Set user principal.
+ */
+public class UserRoleRequestWrapper extends HttpServletRequestWrapper {
+
+    String user=null;
+    HttpServletRequest realRequest;
+  
+    public UserRoleRequestWrapper(String user, HttpServletRequest request) {
+        super(request);
+        this.user = user;
+        this.realRequest = request;
+    }
+
+    @Override
+    public Principal getUserPrincipal() {
+      
+        // anonymous implementation to return user
+        return new Principal() {
+            @Override
+            public String getName() {     
+                return user;
+            }
+        };
+    }
+}
