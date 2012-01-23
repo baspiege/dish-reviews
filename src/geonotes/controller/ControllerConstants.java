88ceaa96410167517d3ddf78127520c12a57f2ee
spiegel121 <spiegel121@gmail.com>
2012-01-23 03:16:30 +0000

    Update use servlet controller.

--- src/geonotes/controller/ControllerConstants.java
+++ src/geonotes/controller/ControllerConstants.java
@@ -12,8 +12,9 @@ public class ControllerConstants {
     public static String STORES_REDIRECT="/storesRedirect.jsp";
     
     // Pages
-    public static String REVIEW_ADD="/reviewAdd.jsp";
-    public static String REVIEW_IMAGE="/reviewImage.jsp";
-    public static String REVIEW_UPDATE="/reviewUpdate.jsp";
-    public static String REVIEW_VOTE="/reviewVote.jsp";
+    public static String REVIEW_ADD="/WEB-INF/pages/reviewAdd.jsp";
+    public static String REVIEW_IMAGE="/WEB-INF/pages/reviewImage.jsp";
+    public static String REVIEW_UPDATE="/WEB-INF/pages/reviewUpdate.jsp";
+    public static String REVIEW_VOTE="/WEB-INF/pages/reviewVote.jsp";
+    public static String REVIEWS_OWN="/WEB-INF/pages/reviewOwn.jsp";
 }
--- src/geonotes/controller/ReviewVoteServlet.java
+++ src/geonotes/controller/ReviewVoteServlet.java
@@ -1,6 +1,5 @@
 package geonotes.controller;
 
-import geonotes.data.PMF;
 import geonotes.data.ReviewGetSingle;
 import geonotes.data.ReviewUpdateYesNo;
 import geonotes.data.ReviewUpdateUndoYesNo;
@@ -9,7 +8,6 @@ import geonotes.utils.RequestUtils;
 import geonotes.utils.StringUtils;
 import java.io.IOException;
 import java.util.ResourceBundle;
-import javax.jdo.PersistenceManager;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
