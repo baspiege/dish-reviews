86fdef8f8ab5ce0288d7fdd2934d8eb0a501b55c
spiegel121@gmail.com <spiegel121@gmail.com@d47931f1-c9d7-4dc6-7dbc-dd6694c20093>
2012-08-19 13:50:14 +0000

    Make store dynamically.

--- trunk/war/js/utils.js
+++ trunk/war/js/utils.js
@@ -95,7 +95,7 @@ function calculateBearing(lat1, lon1, lat2, lon2) {
 }
 
 ///////////////////
-// Display
+// DOM
 ///////////////////
 
 function removeChildrenFromElement(element) {
@@ -104,6 +104,15 @@ function removeChildrenFromElement(element) {
       element.removeChild(element.firstChild);
     }
   }
+}
+
+///////////////////
+// Display
+///////////////////
+
+function elementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (rect.top >= 0 && rect.bottom <= window.innerHeight);
 }
 
 ///////////////////
@@ -119,4 +128,23 @@ function setItemIntoLocalStorage(key, value) {
       localStorage.clear();
     }
   }
+}
+
+///////////////////
+// URL
+///////////////////
+
+function getQueryStrings() {
+  var qsParm = new Array();
+  var query = window.location.search.substring(1);
+  var parms = query.split('&');
+  for (var i=0; i<parms.length; i++) {
+    var pos = parms[i].indexOf('=');
+      if (pos > 0) {
+      var key = parms[i].substring(0,pos);
+      var val = parms[i].substring(pos+1);
+      qsParm[key] = val;
+    }
+  }
+  return qsParm;
 }
